830c716880860a8a9fce501df35f9a7e521b6b5f
Olivia Andrew <andrewoc@dukes.jmu.edu>
2026-09-09 21:03:17 -0400

    settings.py changes for env

--- locallibrary_config/settings.py
+++ locallibrary_config/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+# FYI: OS environment variables take precedence over variables from .env
+env.read_env(str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
