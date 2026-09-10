5ad3a1362211986ae2e4cb3f56c84cd80f7209aa
Olivia Andrew <andrewoc@dukes.jmu.edu>
2026-09-09 21:43:14 -0400

    Fix

--- catalog/admin.py
+++ catalog/admin.py
@@ -43,7 +43,7 @@ class BookAdmin(admin.ModelAdmin):
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -51,6 +51,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back', 'borrower')
+            'fields': ('status', 'due_back')
         }),
     )
